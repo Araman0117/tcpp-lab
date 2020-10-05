a0f095332040827a36c4d2fec3aa5824230af3ad
Roman Nabiev <42548588+Araman0117@users.noreply.github.com>
2020-10-05 19:34:35 +0300

    Add files via upload

--- lab1/laba1.c
+++ lab1/laba1.c
@@ -3,92 +3,128 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int g_array[127];
+int g_array[127] = {0};
 
-int count(char *v)
+int scan_text(char *text)
 {
-    int res = 0;
-    char *str = v;
-    while (*str)
+    //инициализация переменных
+    char filename[] = "testfile.txt";
+    int i = 64;
+    int s_count = strlen(text);
+    int sum = 0;
+    int fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, S_IRWXU);
+    char buffer[s_count];
+    // char *buffer = (char*)malloc(sizeof(char) * (s_count + 1));
+    char *temp;
+
+    //запись в файл из потока ввода
+    write(fd, text, s_count);
+    close(fd);
+
+    //чтение из файла в буфер
+    fd = open(filename, O_RDONLY, S_IRWXU);
+    read(fd, buffer, s_count);
+    close(fd);
+
+    //сбор статистики символов
+    // temp = buffer;
+    temp = text;
+    while (*temp)
     {
-        res++;
-        str++;
+        g_array[*temp - 1] += 1;
+        temp++;
     }
-    return res;
+
+    // printf("%s\ns_count = %d\n", buffer, s_count);
+    // free(buffer);
+
+    //вывод первичной статистики для латин алфавита(кол-во для каждого символа)
+    while (i < 90)
+    {
+        if (g_array[i] + g_array[i + 32] > 0)
+        {
+            printf("'%c' found %d times\n", (i + 1), (g_array[i] + g_array[i + 32]));
+            sum += (g_array[i] + g_array[i + 32]);
+        }
+        i++;
+    }
+
+    //вывод первичной статистики для второстепенных символов(кол-во для каждого символа)
+    i = 32;
+    while (i < 126)
+    {
+        if ( (g_array[i] > 0) && ( (i < 64) || ((i > 89) && (i < 96)) || (i > 121) ) )
+        {
+            printf("'%c' found %d times\n", (i + 1), (g_array[i]));
+            sum += g_array[i];
+        }
+        i++;
+    }
+
+    write(1, "\n", 1);
+
+    return (sum);
+}
+
+void show_stat(int sum)
+{
+    //инициализация переменных
+    int i = 64;
+    int j = 0;
+    float num;
+    float div = (float)(sum);
+
+    //подсчёт процентов и построение гистограмм для латин алфавита
+    while (i < 90)
+    {
+        if (g_array[i] + g_array[i + 32] > 0)
+        {
+            //тут проценты
+            num = (float)((g_array[i] + g_array[i + 32]));
+            printf("'%c' took %f%% of text\t\t", (i + 1), (num * 100 / div));
+
+            //тут гистограммы
+            while (j < (int)(num * 100 / div))
+            {
+                write(1, "*", 1);
+                j++;
+            }
+            j = 0;
+
+            write(1, "*\n", 2);
+        }
+        i++;
+    }
+
+    //подсчёт процентов и построение гистограмм для второстепенных символов
+    i = 32;
+    while (i < 126)
+    {
+        if ( (g_array[i] > 0) && ( (i < 64) || ((i > 89) && (i < 96)) || (i > 121) ) )
+        {
+            num = (float)(g_array[i]);
+            printf("'%c' took %f%% of text\t\t", (i + 1), (num * 100 / div));
+
+            while (j < (int)(num * 100 / div))
+            {
+                write(1, "*", 1);
+                j++;
+            }
+            j = 0;
+
+            write(1, "*\n", 2);
+        }
+        i++;
+    }
 }
 
 int main(int argc, char *argv[])
 {
-    if (argc == 2)
-    {
-        int s_count = count(argv[1]);
-        char filename[] = "testfile.txt";
-        int w_flags = O_WRONLY | O_CREAT;
-        int r_flags = O_RDONLY;
-        mode_t mode = S_IRWXU;
-        int fd = open(filename, w_flags, mode);
-        char *str = argv[1];
-        write(fd, str, s_count);
-        close(fd);
-        //printf("str = %s\ns_count = %d", str, s_count);
-        //printf("amount of symbols - %d\nargc = %d", s_count, argc);
-        fd = open (filename, r_flags, mode);
-        char text[s_count];
-        read(fd, text, s_count);
-        char *temp = text;
-        //printf("text = %s", text);
-        while (*temp)
-        {
-            g_array[*temp - 1] += 1;
-            temp++;
-        }
-
-        int j = 0;
-        int i = 32;
-        while (i < 122)
-        {
-            if (g_array[i])
-            {
-                j++;
-            }
-            i++;
-        }
-
-        i = 64;
-        while (i < 90)
-        {
-            if ((g_array[i]+g_array[i+32]))
-            {
-                printf("num of %c = %d %d\t\t", (i+1), (g_array[i]+g_array[i+32]), ((g_array[i]+g_array[i+32])*100/j));
-                int k = 0;
-                while (k < (g_array[i]+g_array[i+32])*100/j)
-                {
-                    write(1, "*", 1);
-                    k++;
-                }
-                write(1, "\n", 1);
-            }
-            i++;
-        }
-
-        i = 32;
-        while (i < 64)
-        {
-            if (g_array[i])
-            {
-                printf("num of %c = %d %d\t\t", (i+1), (g_array[i]), ((g_array[i])*100/j));
-                int k = 0;
-                while (k < g_array[i]*100/j)
-                {
-                    write(1, "*", 1);
-                    k++;
-                }
-                write(1, "\n", 1);
-            }
-            i++;
-        }
-
-    }
+    if (argc == 2) show_stat(scan_text(argv[1]));
+    //в скан_текст идёт первый параметр в качестве текста для анализа, результат функции идёт в шоу стат для дальнейш обработки
+    else printf("Wrong number of params, insert 1 param instead of %d", (argc - 1)); //обработка ошибки
     return 0;
 }
